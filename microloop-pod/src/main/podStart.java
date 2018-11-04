50a3dc735c20c86d40718fe6f14d115fe7b0524a
Ian Sloat <iansloat537@gmail.com>
2018-11-04 16:02:11 -0600

    yes

--- microloop-pod/src/main/podStart.java
+++ microloop-pod/src/main/podStart.java
@@ -7,7 +7,7 @@ import org.java_websocket.server.WebSocketServer;
 public class podStart {
 
 	public static void main(String[] args) {
-		String host = "";
+		String host = "0.0.0.0";
 		int port = 8887;
 		
 		WebSocketServer server = new wsServer(new InetSocketAddress(host, port));
