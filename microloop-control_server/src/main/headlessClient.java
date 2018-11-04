7e809ff8711444ef05fd0e1062394dec7f71ae52
Ian Sloat <iansloat537@gmail.com>
2018-11-04 15:57:30 -0600

    yes

---
+++ microloop-control_server/src/main/headlessClient.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.java_websocket.client.WebSocketClient;
+
+public class headlessClient {
+
+	public static void main(String[] args) {
+		try {
+			WebSocketClient client = new wsClient(new URI("ws://192.168.31.243:8887:8887"));
+			client.connect();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ microloop-control_server/src/main/wsClient.java
@@ -0,0 +1,37 @@
+package main;
+
+import java.net.URI;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+public class wsClient extends WebSocketClient{
+
+	public wsClient(URI serverURI) {
+		super(serverURI);
+	}
+	
+	@Override
+	public void onClose(int code, String reason, boolean remote) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onError(Exception ex) {
+		System.err.println("an error occurred:" + ex);
+	}
+
+	@Override
+	public void onMessage(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onOpen(ServerHandshake handshakedata) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
